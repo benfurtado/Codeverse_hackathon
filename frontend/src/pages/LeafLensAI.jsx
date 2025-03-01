126771e57791cb5b11ae1ebe5813f7685335434b
benfurtado <raynfurtado@gmail.com>
2025-03-02 01:56:53 +0530

    fixed error

--- frontend/src/pages/LeafLensAI.jsx
+++ frontend/src/pages/LeafLensAI.jsx
@@ -37,7 +37,7 @@ const LeafLensAI = () => {
         });
 
         try {
-            const { data } = await axios.post("http://localhost:5001/identify", formData, {
+            const { data } = await axios.post("http://localhost:5000/api/identify", formData, {
                 headers: { "Content-Type": "multipart/form-data" },
             });
             setResult(data);
